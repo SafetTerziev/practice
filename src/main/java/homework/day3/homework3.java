6df92a520ecb2f6ec12f109dcd4be6aecba1e5c5
SafetTerziev <safetterziev8@gmail.com>
2023-07-05 12:40:33 +0300

    Homework - 2

---
+++ src/main/java/homework/day3/homework3.java
@@ -0,0 +1,38 @@
+package homework.day3;
+
+import java.util.Scanner;
+
+public class homework3 {
+    public static int exercise1(int f, int s, int th){
+        int min = f;
+
+        if (min > s)
+            min = s;
+
+        if (min > th)
+            min = th;
+
+        return min;
+    }
+    public static void exercise2(int number){
+
+        if (number % 2 == 0)
+            System.out.println("true");
+
+        else if (number % 2 != 0)
+            System.out.println("false");
+
+
+    }
+    public static void main(String[] args) {
+        //Homework day 3
+        //zad 1
+        System.out.println("Min is " + exercise1(3,4,2));
+
+        //zad 2
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = num.nextInt();
+        exercise2(n);
+    }
+}
